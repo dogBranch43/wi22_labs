653a8a563a29ee253842caa32f04faa60eb2a68b
dogBranch43 <97578072+dogBranch43@users.noreply.github.com>
2022-01-13 17:59:02 -0800

    Add files via upload

--- lab1/multiplexor64of32to1.sv
+++ lab1/multiplexor64of32to1.sv
@@ -1,21 +1,26 @@
-module multiplexor64of32to1(in, out, select) ;
-	input logic [4:0] select;
-	input logic [][] in;
-	output logic [63:0];
+module multiplexor64of32to1(in, readData1, readData2, readRegister1, readRegister2) ;
+	input logic [4:0] readRegister1, readRegister2;
+	input logic [31:0][63:0] in;
+	output logic [63:0] readData1, readData2;
 	//registers are aligned 32 x 64 bits of data
 	//still incomplete;
-	genvar i;
-	generate
-		for (int i = 0; i < 64; i++) begin
-			multiplexor32to1 first_mux64x32to1 (in[][], out[i], select);
-		end
-	endgenerate
+	logic [63:0][31:0] flipped;
 	
-	genvar j;
-	generate
-		for (int j = 0; j < 64; j++) begin
-			multiplexor32to1 second_mux64x32to1 (in[][], out[j], select);
-		end
-	endgenerate
-	
+	 genvar i, j;
+
+    generate 
+        for (i = 0; i < 64; i++) begin : outerFlip
+            for (j = 0; j < 32; j++) begin : innerFlip
+                flipped[i][j] = in[j][i];
+            end
+        end
+    endgenerate
+
+	 genvar k;
+    generate
+        for (k = 0; k < 64; k++) begin : makeChain
+            multiplexor32to1 mp1 (flipped[k][31:0], readData1[k], readRegister1);
+            multiplexor32to1 mp2 (flipped[k][31:0], readData2[k], readRegister2);
+        end
+    endgenerate
 endmodule
--- lab1/regfile.sv
+++ lab1/regfile.sv
@@ -6,6 +6,10 @@ module regfile(ReadData1, ReadData2, WriteData,
 	input logic 		RegWrite, clk;
 	output logic [63:0]	ReadData1, ReadData2;
 	
+	logic [31:0][63:0] registers;
+   logic [31:0] enableDecoders, curRead1Register, curRead2Register;
+	assign registers[31] = 64'b0;
+	
 	//main method
 	//regwrite and write register goes to the decoder
 	
@@ -15,8 +19,25 @@ module regfile(ReadData1, ReadData2, WriteData,
 	
 	//read register 1 and 2 goes to read data
 	
-
 	
-
+	logic reset;
+	//decoder
+   decoder5to32 d1(.in(WriteRegister), .out(enableDecoders), .enable(RegWrite));
 	
+	//64x32:1 mux
+	multiplexor64of32to1 chain64 (.in(registers), .readData1(ReadData1), .readData2(ReadData2), .readRegister1(ReadRegister1), .readRegister2(ReadRegister2));
+	
+    //registers
+	genvar i,j;
+   generate 
+       for (i = 0; i < 32; i++) begin : register
+           for  (j = 0; j < 64; j++) begin : multiplexo
+					logic [1:0] temp;
+					logic temp2;
+					assign temp[1] = registers[i][j];	
+					muxiplexor2to1 m1(.in(temp), .out(temp2), .select(enableDecoders[i]));
+               D_FF dff1(.q(temp2), .d(temp[0]), .reset, .clk);
+           end
+       end
+   endgenerate
 endmodule
