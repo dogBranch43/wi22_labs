30236bdf7549fa4bfc4fcef896a684af9e8dd328
jongjam <jongjam64@gmail.com>
2022-02-03 14:42:08 -0800

    testing for control signals done

--- lab3/controlSignals.sv
+++ lab3/controlSignals.sv
@@ -5,7 +5,7 @@
 //This is the control signals method
 //Based on the inputed instructions, it will select the correct control signals
 //to be used in the CPU
-`timescale 1ns/10ps
+`timescale 1ps/1ps
 module controlSignals(instruction, zero, negative, overflow, carry_out, Reg2Loc, ALUSrc, MemToReg, RegWrite, MemWrite, BrTaken, UncondBr, ALUOp);
 	input logic [31:0] instruction;
 	input logic zero, negative, carry_out, overflow;
@@ -17,7 +17,7 @@ module controlSignals(instruction, zero, negative, overflow, carry_out, Reg2Loc,
 						  ADDS = 11'b10101011000, LSR = 11'b11010011010, AND = 11'b10001010000, BLT = 11'b01010100xxx;
 	//Selecting the correct signals based on the input
 	always_comb begin
-		case(instruction)
+		case(instruction[31:21])
 			 ADDI : begin
 										Reg2Loc   = 1'bx;
 										ALUSrc    = 1'b1;
@@ -149,40 +149,41 @@ module controlSignals_testbench() ;
 	controlSignals dut (instruction, zero, negative, overflow, carry_out, Reg2Loc, ALUSrc, MemToReg, RegWrite, MemWrite, BrTaken, UncondBr, ALUOp);
 	
 	initial begin
-	//LDUR
-	instruction = 32'b11111000010xxxxxxxxxxxxxxxxxxxxx; #20;
-	//ADDI	
-	instruction = 32'b1001000100xxxxxxxxxxxxxxxxxxxxxx; #20;
-	//ADDS
-	instruction = 32'b10101011000xxxxxxxxxxxxxxxxxxxxx; #20;
-	//SUBS
-	instruction = 32'b11101011000xxxxxxxxxxxxxxxxxxxxx; #20;
-	//LSR
-	instruction = 32'b11010011100xxxxxxxxxxxxxxxxxxxxx; #20;
- 	//CBZ
-	zero = 0;
-	instruction = 32'b10110100xxxxxxxxxxxxxxxxxxxxxxxx; #20;         //CBZ
-	zero = 1;
-	instruction = 32'b10110100xxxxxxxxxxxxxxxxxxxxxxxx; #20;         //CBZ
-	//B
-	zero = 0;
-	instruction = 32'b000101xxxxxxxxxxxxxxxxxxxxxxxxxx; #20;
-	//B.LT
-	negative = 0; overflow = 0; #20;
-	instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #20;
-	negative = 0; overflow = 1; #20;
-	instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #20;
-	negative = 1; overflow = 0; #20;                   
-	instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #20;
-	negative = 1; overflow = 1; #20;                   
-	instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #20;
-	//EOR                                              
-	instruction = 32'b11001010000xxxxxxxxxxxxxxxxxxxxx; #20;
-	//AND                                              
-	instruction = 32'b10001010000xxxxxxxxxxxxxxxxxxxxx; #20;
-	//STUR
-	instruction = 32'b11111000000xxxxxxxxxxxxxxxxxxxxx; #20;
-	//SUBS                                              
-	instruction = 32'b11101011000xxxxxxxxxxxxxxxxxxxxx; #20;
+		//LDUR
+		instruction = 32'b11111000010xxxxxxxxxxxxxxxxxxxxx; #2000;
+		//ADDI	
+		instruction = 32'b1001000100xxxxxxxxxxxxxxxxxxxxxx; #2000;
+		//ADDS
+		instruction = 32'b10101011000xxxxxxxxxxxxxxxxxxxxx; #2000;
+		//SUBS
+		instruction = 32'b11101011000xxxxxxxxxxxxxxxxxxxxx; #2000;
+		//LSR
+		instruction = 32'b11010011100xxxxxxxxxxxxxxxxxxxxx; #2000;
+		//CBZ
+		zero = 0;
+		instruction = 32'b10110100xxxxxxxxxxxxxxxxxxxxxxxx; #2000;         //CBZ
+		zero = 1;
+		instruction = 32'b10110100xxxxxxxxxxxxxxxxxxxxxxxx; #2000;         //CBZ
+		//B
+		zero = 0;
+		instruction = 32'b000101xxxxxxxxxxxxxxxxxxxxxxxxxx; #2000;
+		//B.LT
+		negative = 0; overflow = 0; #20;
+		instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #2000;
+		negative = 0; overflow = 1; #20;
+		instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #2000;
+		negative = 1; overflow = 0; #20;                   
+		instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #2000;
+		negative = 1; overflow = 1; #20;                   
+		instruction = 32'b01010100xxxxxxxxxxxxxxxxxxxxxxxx; #2000;
+		//EOR                                              
+		instruction = 32'b11001010000xxxxxxxxxxxxxxxxxxxxx; #2000;
+		//AND                                              
+		instruction = 32'b10001010000xxxxxxxxxxxxxxxxxxxxx; #2000;
+		//STUR
+		instruction = 32'b11111000000xxxxxxxxxxxxxxxxxxxxx; #2000;
+		//SUBS                                              
+		instruction = 32'b11101011000xxxxxxxxxxxxxxxxxxxxx; #2000;
+		$stop;
 	end
 endmodule
--- lab3/controlSignals_wave.do
+++ lab3/controlSignals_wave.do
@@ -1,18 +1,29 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
 add wave -noupdate /controlSignals_testbench/instruction
+add wave -noupdate /controlSignals_testbench/dut/LDUR
+add wave -noupdate /controlSignals_testbench/dut/ADDI
+add wave -noupdate /controlSignals_testbench/dut/B
+add wave -noupdate /controlSignals_testbench/dut/SUBS
+add wave -noupdate /controlSignals_testbench/dut/EOR
+add wave -noupdate /controlSignals_testbench/dut/CBZ
+add wave -noupdate /controlSignals_testbench/dut/STUR
+add wave -noupdate /controlSignals_testbench/dut/ADDS
+add wave -noupdate /controlSignals_testbench/dut/LSR
+add wave -noupdate /controlSignals_testbench/dut/AND
+add wave -noupdate /controlSignals_testbench/dut/BLT
 add wave -noupdate /controlSignals_testbench/zero
 add wave -noupdate /controlSignals_testbench/negative
 add wave -noupdate /controlSignals_testbench/carry_out
 add wave -noupdate /controlSignals_testbench/overflow
-add wave -noupdate /controlSignals_testbench/Reg2Loc
-add wave -noupdate /controlSignals_testbench/ALUSrc
-add wave -noupdate /controlSignals_testbench/MemToReg
-add wave -noupdate /controlSignals_testbench/RegWrite
-add wave -noupdate /controlSignals_testbench/MemWrite
-add wave -noupdate /controlSignals_testbench/BrTaken
-add wave -noupdate /controlSignals_testbench/UncondBr
-add wave -noupdate /controlSignals_testbench/ALUOp
+add wave -noupdate /controlSignals_testbench/dut/Reg2Loc
+add wave -noupdate /controlSignals_testbench/dut/ALUSrc
+add wave -noupdate /controlSignals_testbench/dut/MemToReg
+add wave -noupdate /controlSignals_testbench/dut/RegWrite
+add wave -noupdate /controlSignals_testbench/dut/MemWrite
+add wave -noupdate /controlSignals_testbench/dut/BrTaken
+add wave -noupdate /controlSignals_testbench/dut/UncondBr
+add wave -noupdate /controlSignals_testbench/dut/ALUOp
 TreeUpdate [SetDefaultTree]
 WaveRestoreCursors {{Cursor 1} {0 ps} 0}
 quietly wave cursor active 1
@@ -30,4 +41,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {0 ps} {1 ns}
+WaveRestoreZoom {0 ps} {33684 ps}
