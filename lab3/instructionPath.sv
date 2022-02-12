9500b7692ce11a02b5a0b907938e67ea8c6beb41
dogBranch43 <97578072+dogBranch43@users.noreply.github.com>
2022-02-11 23:52:49 -0800

    Update instructionPath.sv

--- lab3/instructionPath.sv
+++ lab3/instructionPath.sv
@@ -17,6 +17,7 @@ module instructionPath(clk, BrTaken, UncondBr, instruction, rst) ;
 	logic [63:0] PC;
 	logic [31:0] instructionTemp;
 	
+	//extender as seen in diagram for the branches.
 	signExtender #(26) unBr (.addr(instructionTemp[25 : 0]), .out(uncondAddr26));
 	signExtender #(19) cbz (.addr(instructionTemp[23 : 5]), .out(condAddr19));
 		
@@ -27,18 +28,21 @@ module instructionPath(clk, BrTaken, UncondBr, instruction, rst) ;
 	//need two sign extenders
 	
 	DFFs_64 dffs (.clk, .rst(1'b0), .in(pcTemp), .out(pcPrev));
-
+	
+	//shifting the branch left by 2
 	assign shiftBrAddr = BrAddr << 2;
 	
 	//for addigng PC insturctions
 	fullAdder_64bit plus4 (.a(PC), .b(64'd4), .out(noBranchTemp));
 	fullAdder_64bit plusBr (.a(pcPrev), .b(shiftBrAddr), .out(BrTakenTemp));
 	
+	//Muxes: 1st chooses uncondbr and 2nd chooses a branch taken or not, not is for pc <= pc + 4;
 	mux64_2to1 m1uncondBr(.i0(uncondAddr26), .i1(condAddr19), .out(BrAddr), .select(UncondBr));
 	mux64_2to1 m2BrTaken(.i0(noBranchTemp), .i1(BrTakenTemp), .out(pcTemp), .select(BrTaken));
 	
 	instructmem 		im1 (.address(PC), .instruction(instructionTemp), .clk);
 	
+	//this block increments PC and instruction accordingly
 	always_ff @(posedge clk) begin
 		if (rst) begin
 			PC <= 0;
