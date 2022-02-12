b8f4b853a40b5a54c0e8125ef48c7f5286b312b0
jxkkim <49467824+jxkkim@users.noreply.github.com>
2022-02-11 23:44:08 -0800

    Update instructionPath.sv

--- lab3/instructionPath.sv
+++ lab3/instructionPath.sv
@@ -1,24 +1,24 @@
-`timescale 10ns/1ps
+`timescale 1ns/10ps
 //This module is the instructionPath
-module instructionPath(clk, BrTaken, UncondBr, instruction, PC) ;
-	input logic [31:0] instruction;
-	input logic clk, BrTaken, UncondBr;
-	output logic [63:0] PC;
+module instructionPath(clk, BrTaken, UncondBr, instruction, rst) ;
+	output logic [31:0] instruction;
+	input logic clk, BrTaken, UncondBr, rst;
+	//output logic [63:0] PC;
 	//Sign 
 	//Default to length 9 opcode for arithmetic
 	//Uncond Br 26
 	//CBZ conditional br 19
 	logic[63:0] uncondAddr26, condAddr19;
 	logic[63:0] BrAddr;
-	logic[63:0] condBrTemp;
 	logic[63:0] BrTakenTemp, noBranchTemp;
 	logic[63:0] pcTemp;
 	logic[63:0] pcPrev;
 	logic[63:0] shiftBrAddr;
+	logic [63:0] PC;
+	logic [31:0] instructionTemp;
 	
-
-	signExtender #(26) unBr (.addr(instruction[25 : 0]), .out(uncondAddr26));
-	signExtender #(19) cbz (.addr(instruction[23 : 5]), .out(condAddr19));
+	signExtender #(26) unBr (.addr(instructionTemp[25 : 0]), .out(uncondAddr26));
+	signExtender #(19) cbz (.addr(instructionTemp[23 : 5]), .out(condAddr19));
 		
 	
 	//Need 1 shifter to two places
@@ -26,23 +26,41 @@ module instructionPath(clk, BrTaken, UncondBr, instruction, PC) ;
 	//need two muxs ( 1 takes BrTaken as signal, other takes unCondBr as signal
 	//need two sign extenders
 	
-	mux64_2to1 m1uncondBr(.i0(uncondAddr26), .i1(condAddr19), .out(BrAddr), .select(UncondBr));
-	mux64_2to1 m2BrTaken(.i0(BrTakenTemp), .i1(noBranchTemp), .out(pcTemp), .select(BrTaken));
-	DFFs_64 dffs (.clk, .rst(1'b0), .in(PC), .out(pcPrev));
+	DFFs_64 dffs (.clk, .rst(1'b0), .in(pcTemp), .out(pcPrev));
 
-//	shifter branchLeftShift2 (.value(BrAddr), .direction(1'b0), .distance(6'b000010), .result(shiftBrAddr));
+	assign shiftBrAddr = BrAddr << 2;
 	
 	//for addigng PC insturctions
 	fullAdder_64bit plus4 (.a(PC), .b(64'd4), .out(noBranchTemp));
 	fullAdder_64bit plusBr (.a(pcPrev), .b(shiftBrAddr), .out(BrTakenTemp));
+	
+	mux64_2to1 m1uncondBr(.i0(uncondAddr26), .i1(condAddr19), .out(BrAddr), .select(UncondBr));
+	mux64_2to1 m2BrTaken(.i0(noBranchTemp), .i1(BrTakenTemp), .out(pcTemp), .select(BrTaken));
+	
+	instructmem 		im1 (.address(PC), .instruction(instructionTemp), .clk);
+	
+	always_ff @(posedge clk) begin
+		if (rst) begin
+			PC <= 0;
+			pcTemp <=0;
+			pcPrev <=0;
+			noBranchTemp<=0;
+			BrTakenTemp <=0;
+			instructionTemp <= 0;
+			BrAddr <=0;
+		end else begin
+			PC <= pcTemp;
+			instruction <= instructionTemp;
+		end
+	end
 endmodule
 
 module instructionPath_testbench() ; 
 	logic [31:0] instruction;
-	logic clk, BrTaken, UncondBr;
+	logic clk, BrTaken, UncondBr, rst;
 	logic [63:0] PC;
 
-	instructionPath dut (.clk, .BrTaken, .UncondBr, .instruction, .PC);
+	instructionPath dut (.clk, .BrTaken, .UncondBr, .instruction, .rst);
 
 	parameter CLOCK_PERIOD = 100000;
 	
@@ -52,8 +70,14 @@ module instructionPath_testbench() ;
 	end
 
 	initial begin
+		rst <=0 ;@(posedge clk);
+		rst <=1 ;@(posedge clk);
+		rst <=0 ;@(posedge clk);
 		instruction <= 32'b10010001000000000110001111100010; BrTaken <= 0; UncondBr <= 0; @(posedge clk);
 		instruction <= 32'b10001011000001010000000001000011; BrTaken <= 0; UncondBr <= 0; @(posedge clk);
+		instruction <= 32'b10001011000001010001100001000011; BrTaken <= 0; UncondBr <= 0; @(posedge clk);
+		instruction <= 32'b10001011000001011000000001000011; BrTaken <= 0; UncondBr <= 0; @(posedge clk);
+		instruction <= 32'b10001011000001010000000101000011; BrTaken <= 0; UncondBr <= 0; @(posedge clk);
 		$stop;
 	end
 endmodule
